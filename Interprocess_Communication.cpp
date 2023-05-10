fe2176051fc3ca1c3cbaac18b5d3476725c05b19
JoseGuzman02 <83370645+JoseGuzman02@users.noreply.github.com>
2023-05-10 14:01:31 -0400

    Update Interprocess_Communication.cpp

--- Interprocess_Communication.cpp
+++ Interprocess_Communication.cpp
@@ -8,8 +8,9 @@ using namespace std;
 
 int main() {
     int fd1[2], fd2[2];
-    int N = 6;
-    int partial_sum = 0;
+    int N;
+    cout << "Enter in a number: ";
+    cin >> N;
 
     if (pipe(fd1) == -1 || pipe(fd2) == -1) {
         cerr << "Failed to create pipes." << endl;
@@ -17,22 +18,24 @@ int main() {
     }
 
     pid_t pid = fork();
-
     if (pid == -1) {
         cerr << "Failed to fork." << endl;
         return 1;
     } else if (pid == 0) {
         // Child process
-
         close(fd1[1]);
         close(fd2[0]);
 
         int sum = 0;
         int value;
-        while (read(fd1[0], &value, sizeof(value))) {
-            sum += value;
+        while (read(fd1[0], &value, sizeof(value))) {}
+
+        cout << "Child reads: " << value << " from Parent" << endl;
+        
+        // compute partial sum of (N/2) to N
+        for(int i = value; i <= N; i++){
+            sum += i;
         }
-        cout << "Child writes: " << sum << endl;
         write(fd2[1], &sum, sizeof(sum));
 
         // Close used pipe ends
@@ -42,23 +45,25 @@ int main() {
         return 0;
     } else {
         // Parent process
-
         close(fd1[0]);
         close(fd2[1]);
-
-        for (int i = 1; i <= N; i++) {
-            partial_sum += i;
-            write(fd1[1], &i, sizeof(i));
-        }
-
+        
+        int partial_sum = 0;
+        int n = N / 2;
+        write(fd1[1], &n, sizeof(n));
         close(fd1[1]);
-
+        
         int child_sum;
         read(fd2[0], &child_sum, sizeof(child_sum));
-        cout << "Parent reads: " << child_sum << endl;
+        cout << "Parent reads: " << child_sum << " from child" << endl;
+        
+        // Computes partial sum of 1 to (N/2 - 1)
+        for (int i = 1; i < n; i++){
+            partial_sum += i;
+        }
 
         int total_sum = partial_sum + child_sum;
-        cout << "The sum of 1 through " << N << " is " << total_sum << endl;
+        cout << "The partial sum of 1 through " << N << " is " << total_sum << endl;
 
         // Close used pipe ends
         close(fd2[0]);
